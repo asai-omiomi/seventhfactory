d54b2de617c7181c22eaad8193292eb9441fca47
imoeddie <asaiomiomi@gmail.com>
2024-12-01 21:51:34 +0900

    デプロイ用（切り替え機能導入）

--- seventhsystem/settings.py
+++ seventhsystem/settings.py
@@ -85,16 +85,18 @@ WSGI_APPLICATION = "seventhsystem.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if ENVIRONMENT == "development":
+
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
 else:
+
     default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+
     DATABASES = {
         "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
     }
@@ -134,18 +136,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
 
-
-if ENVIRONMENT == "development":
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+if DEBUG:
+    STATIC_URL = "static/"
 
 else:
-    # STATIC_ROOT = str(BASE_DIR / "staticfiles")
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"    
+    STATIC_URL = "static/"
+    STATIC_ROOT = str(BASE_DIR / "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -155,3 +153,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"  # ログインページ
 LOGIN_REDIRECT_URL = "/info_today"  # ログイン後のリダイレクト先
 LOGOUT_REDIRECT_URL = "/"  # ログアウト後のリダイレクト先
+
+
+SUPERUSER_NAME = env("SUPERUSER_NAME")
+SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
+SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
